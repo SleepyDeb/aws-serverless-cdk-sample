f7722b66e9b801da792daedfb3467aaf2c2ca10b
null <alessio.dimaria@gmail.com>
2023-03-16 15:56:22 +0100

    adding git ignore

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,19 +1,34 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamo from 'aws-cdk-lib/aws-dynamodb';
+import * as path from 'path';
+import * as fs from 'fs';
+
+const lambdaCodeDirectory = path.resolve(__dirname, '../../', 'app/backend/dist');
+
+if(!fs.existsSync(lambdaCodeDirectory))
+  throw new Error(`Please build the code directory with 'npm run build' first (${lambdaCodeDirectory})`);
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const ordersTable = new dynamo.Table(this, `orders-table`, {
+      tableName: `sample-orders-table`,
+      partitionKey: {
+        name: 'id',
+        type: dynamo.AttributeType.STRING
+      }
     });
 
-    const topic = new sns.Topic(this, 'CdkTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const handler = new lambda.Function(this, "PutOrderLambda", {
+      runtime: lambda.Runtime.NODEJS_14_X, // So we can use async in widget.js
+      code: lambda.Code.fromAsset("resources"),
+      handler: "handlers.putOrder",
+      environment: {
+        ORDERS_TABLE_NAME: ordersTable.tableName
+      }
+    })
   }
 }
