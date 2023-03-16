5408ff19255a9c4f6194f128c0372b5a19c75154
null <alessio.dimaria@gmail.com>
2023-03-16 16:48:09 +0100

    implementing putOrder demo

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -4,12 +4,18 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as dynamo from 'aws-cdk-lib/aws-dynamodb';
 import * as path from 'path';
 import * as fs from 'fs';
+import * as fsex from 'fs-extra';
 
-const lambdaCodeDirectory = path.resolve(__dirname, '../../', 'app/backend/dist');
+const lambdaCodeDirectory = path.resolve(__dirname, '../../', 'app/backend/dist/src');
 const lambdaCodeAsset = lambda.Code.fromAsset(lambdaCodeDirectory);
-
 if(!fs.existsSync(lambdaCodeDirectory))
   throw new Error(`Please build the code directory with 'npm run build' first (${lambdaCodeDirectory})`);
+  
+const lambdaNodeModulesDirectory = path.resolve(__dirname, '../../', 'app/backend/node_modules/');
+const lambdaNodeModulesDirectoryDestination = path.resolve(__dirname, '../../', 'app/backend/layer/nodejs/node_modules/');
+const lambdaNodeModulesDirectoryLayerDirectory = path.resolve(__dirname, '../../', 'app/backend/layer/');
+fsex.copySync(lambdaNodeModulesDirectory, lambdaNodeModulesDirectoryDestination, { overwrite: true });
+const lambdaLayerAsset = lambda.Code.fromAsset(lambdaNodeModulesDirectoryLayerDirectory);
 
 export class CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -23,10 +29,16 @@ export class CdkStack extends Stack {
       }
     });
 
+    const basicLambdaLayer = new lambda.LayerVersion(this, `NodeModulesDepsLayer`, {
+      compatibleRuntimes: [ lambda.Runtime.NODEJS_18_X ],
+      code: lambdaLayerAsset
+    })
+
     const putOrderHandler = new lambda.Function(this, "PutOrderLambda", {
-      runtime: lambda.Runtime.NODEJS_14_X, // So we can use async in widget.js
+      runtime: lambda.Runtime.NODEJS_18_X,
       code: lambdaCodeAsset,
       handler: "handlers.putOrder",
+      layers: [ basicLambdaLayer ],
       environment: {
         ORDERS_TABLE_NAME: ordersTable.tableName
       }
